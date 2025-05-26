ded48eb869cf15283102d5b74d72c25638dd6aff
David Akinola <dakinola54@gmail.com>
2025-05-26 22:44:31 +0100

    corrections on pattern making

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,8 @@
 size = int(input("Enter the size of the pattern: "))
-number_of_rows = 0
+number_of_rows = 1
 
-while number_of_rows < size:
-    for i in range(size):
-        print("*" * size)
-        number_of_rows += 1
+while number_of_rows <= size:
+    for _ in range(size):
+        print("*", end="")
+    print("")
+    number_of_rows += 1
