acc86a127a0217afc6a2fb4b5e6733e9a0b94901
David Akinola <dakinola54@gmail.com>
2025-06-04 15:11:59 +0100

    correction

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
 def perform_operation(num1, num2, operation):
     if num2 == 0:
         return "Cannot divide by zero"
-    if operation == '+':
+    if operation == 'add':
         return num1 + num2
-    elif operation == '-':
+    elif operation == 'subtract':
         return num1 - num2
-    elif operation == '*':
+    elif operation == 'multiply':
         return num1 * num2
-    elif operation == '/':
+    elif operation == 'divide':
         return num1 / num2
 
