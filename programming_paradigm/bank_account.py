5fda45e51a6f6d17fd4ee85ae50085b7b87a4e5d
David Akinola <dakinola54@gmail.com>
2025-06-12 11:20:30 +0200

    recolved failing check ✔

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,4 +1,4 @@
-class BankAccount():
+class BankAccount:
     def __init__(self, account_balance, initial_balance=0):
         self.account_balance = account_balance
         self.initial_balance = initial_balance
