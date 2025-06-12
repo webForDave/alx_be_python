33646fc033edb06dc28d6a54fec85d272a188ae0
David Akinola <dakinola54@gmail.com>
2025-06-12 11:12:34 +0200

    modelling a banking service

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount():
+    def __init__(self, account_balance, initial_balance=0):
+        self.account_balance = account_balance
+        self.initial_balance = initial_balance
+
+    def deposit(self, amount):
+        return self.account_balance + amount
+
+    def withdraw(self, amount):
+        if self.account_balance > amount:
+            self.account_balance - amount
+            return True
+        else:
+            return False
+        
+    def display_balance(self):
+        return f"Current Balance: ${self.account_balance}"
